4238952928ef73261e5177d6fe24f62a6eb3750a
yangxuan <xuan.yang@zilliz.com>
2023-05-10 18:11:15 +0800

    Add dataset iterator
    - Add VectorDB api and Milvus client
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- tests/test_case.py
+++ tests/test_case.py
@@ -4,11 +4,10 @@ from falcon_mark.backend import cases
 
 log  = logging.getLogger(__name__)
 class TestCases:
-    @pytest.mark.skip()
+    #  @pytest.mark.skip()
     def test_init_LoadCase(self):
         c = cases.LoadSDimCase(run_id=1)
-        log.debug(f"c: {c}")
-
+        log.debug(f"c: {c}, {c.model_dump().keys()}")
 
     def test_case_type(self):
         from falcon_mark.models import CaseType
