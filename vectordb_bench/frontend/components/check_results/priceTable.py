611ea30c15fd66ed3a1a506f85e0f10ac2cfa5c2
min.tian <min.tian.cn@gmail.com>
2023-06-14 18:51:17 +0800

    [front] add editable price table
    Signed-off-by: min.tian <min.tian.cn@gmail.com>

---
+++ vectordb_bench/frontend/components/check_results/priceTable.py
@@ -0,0 +1,48 @@
+from vectordb_bench.backend.clients import DB
+from vectordb_bench.frontend.const import DB_DBLABEL_TO_PRICE
+import pandas as pd
+from collections import defaultdict
+import streamlit as st
+
+
+def priceTable(container, data):
+    dbAndLabelSet = {
+        (d["db"], d["db_label"]) for d in data if d["db"] != DB.Milvus.value
+    }
+
+    dbAndLabelList = list(dbAndLabelSet)
+    dbAndLabelList.sort()
+
+    table = pd.DataFrame(
+        [
+            {
+                "DB": db,
+                "Label": db_label,
+                "Price per hour": DB_DBLABEL_TO_PRICE.get(db, {}).get(db_label, 0),
+            }
+            for db, db_label in dbAndLabelList
+        ]
+    )
+    height = len(table) * 35 + 38
+
+    expander = container.expander("You can edit the price.")
+    editTable = expander.data_editor(
+        table,
+        use_container_width=True,
+        hide_index=True,
+        height=height,
+        disabled=("DB", "Label"),
+        column_config={
+            "Price per hour": st.column_config.NumberColumn(
+                min_value=0,
+                format="$ %f",
+            )
+        },
+    )
+
+    priceMap = defaultdict(lambda: defaultdict(float))
+    for _, row in editTable.iterrows():
+        db, db_label, price = row
+        priceMap[db][db_label] = price
+
+    return priceMap
