b987180aa0161619fd3b48bd81ed4c1669005253
yangxuan <xuan.yang@zilliz.com>
2023-06-21 15:30:05 +0800

    Remove 100K Performance cases
    We use 100K for correctness and convenience of the code, but the results
    are actually not representative. So we move the 100K performance
    cases into unittests, and remove all results related to it.
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- vectordb_bench/backend/cases.py
+++ vectordb_bench/backend/cases.py
@@ -30,7 +30,6 @@ class CaseType(Enum):
     Performance100M = "Search Performance Test (100M Dataset, 768 Dim)"
     PerformanceLZero = "Search Performance Test (10M Dataset, 768 Dim)"
     PerformanceMZero = "Search Performance Test (1M Dataset, 768 Dim)"
-    PerformanceSZero = "Search Performance Test (100K Dataset, 768 Dim)"
 
     PerformanceLLow = (
         "Filtering Search Performance Test (10M Dataset, 768 Dim, Filter 1%)"
@@ -38,17 +37,11 @@ class CaseType(Enum):
     PerformanceMLow = (
         "Filtering Search Performance Test (1M Dataset, 768 Dim, Filter 1%)"
     )
-    PerformanceSLow = (
-        "Filtering Search Performance Test (100K Dataset, 768 Dim, Filter 1%)"
-    )
     PerformanceLHigh = (
         "Filtering Search Performance Test (10M Dataset, 768 Dim, Filter 99%)"
     )
     PerformanceMHigh = (
         "Filtering Search Performance Test (1M Dataset, 768 Dim, Filter 99%)"
-    )
-    PerformanceSHigh = (
-        "Filtering Search Performance Test (100K Dataset, 768 Dim, Filter 99%)"
     )
 
     def get(self) -> Case:
@@ -105,7 +98,7 @@ class CapacityLDimCase(CapacityCase):
     case_id: CaseType = CaseType.CapacityLDim
     dataset: ds.DataSet = ds.get(ds.Name.GIST, ds.Label.SMALL)
     name: str = "Capacity Test (960 Dim Repeated)"
-    description: str = """This case tests the vector database's loading capacity by repeatedly inserting large-dimension vectors (GIST 100K vectors, <b>960 dimensions</b>) until it is fully loaded.  
+    description: str = """This case tests the vector database's loading capacity by repeatedly inserting large-dimension vectors (GIST 100K vectors, <b>960 dimensions</b>) until it is fully loaded.
 Number of inserted vectors will be reported."""
 
 
@@ -113,7 +106,7 @@ class CapacitySDimCase(CapacityCase):
     case_id: CaseType = CaseType.CapacitySDim
     dataset: ds.DataSet = ds.get(ds.Name.SIFT, ds.Label.SMALL)
     name: str = "Capacity Test (128 Dim Repeated)"
-    description: str = """This case tests the vector database's loading capacity by repeatedly inserting small-dimension vectors (SIFT 100K vectors, <b>128 dimensions</b>) until it is fully loaded.  
+    description: str = """This case tests the vector database's loading capacity by repeatedly inserting small-dimension vectors (SIFT 100K vectors, <b>128 dimensions</b>) until it is fully loaded.
 Number of inserted vectors will be reported."""
 
 
@@ -130,14 +123,6 @@ class PerformanceMZero(PerformanceCase):
     dataset: ds.DataSet = ds.get(ds.Name.Cohere, ds.Label.MEDIUM)
     name: str = "Search Performance Test (1M Dataset, 768 Dim)"
     description: str = """This case tests the search performance of a vector database with a medium dataset (<b>Cohere 1M vectors</b>, 768 dimensions) at varying parallel levels.
-Results will show index building time, recall, and maximum QPS."""
-
-
-class PerformanceSZero(PerformanceCase):
-    case_id: CaseType = CaseType.PerformanceSZero
-    dataset: ds.DataSet = ds.get(ds.Name.Cohere, ds.Label.SMALL)
-    name: str = "Search Performance Test (100K Dataset, 768 Dim)"
-    description: str = """This case tests the search performance of a vector database with a small dataset (<b>Cohere 100K vectors</b>, 768 dimensions) at varying parallel levels.
 Results will show index building time, recall, and maximum QPS."""
 
 
@@ -159,17 +144,6 @@ class PerformanceMLow(PerformanceCase):
 Results will show index building time, recall, and maximum QPS."""
 
 
-class PerformanceSLow(PerformanceCase):
-    case_id: CaseType = CaseType.PerformanceSLow
-    filter_rate: float | int | None = 0.01
-    dataset: ds.DataSet = ds.get(ds.Name.Cohere, ds.Label.SMALL)
-    name: str = "Filtering Search Performance Test (100K Dataset, 768 Dim, Filter 1%)"
-    description: str = (
-        """This case tests the search performance of a vector database with a small dataset (<b>Cohere 100K vectors</b>, 768 dimensions) under a low filtering rate (<b>1% vectors</b>), at varying parallel levels.
-Results will show index building time, recall, and maximum QPS.""",
-    )
-
-
 class PerformanceLHigh(PerformanceCase):
     case_id: CaseType = CaseType.PerformanceLHigh
     filter_rate: float | int | None = 0.99
@@ -188,14 +162,6 @@ class PerformanceMHigh(PerformanceCase):
 Results will show index building time, recall, and maximum QPS."""
 
 
-class PerformanceSHigh(PerformanceCase):
-    case_id: CaseType = CaseType.PerformanceSHigh
-    filter_rate: float | int | None = 0.99
-    dataset: ds.DataSet = ds.get(ds.Name.Cohere, ds.Label.SMALL)
-    name: str = "Filtering Search Performance Test (100K Dataset, 768 Dim, Filter 99%)"
-    description: str = """This case tests the search performance of a vector database with a small dataset (<b>Cohere 100K vectors</b>, 768 dimensions) under a high filtering rate (<b>99% vectors</b>), at varying parallel levels.
-Results will show index building time, recall, and maximum QPS."""
-
 
 class Performance100M(PerformanceCase):
     case_id: CaseType = CaseType.Performance100M
@@ -213,12 +179,9 @@ type2case = {
     CaseType.Performance100M: Performance100M,
     CaseType.PerformanceLZero: PerformanceLZero,
     CaseType.PerformanceMZero: PerformanceMZero,
-    CaseType.PerformanceSZero: PerformanceSZero,
 
     CaseType.PerformanceLLow: PerformanceLLow,
     CaseType.PerformanceMLow: PerformanceMLow,
-    CaseType.PerformanceSLow: PerformanceSLow,
     CaseType.PerformanceLHigh: PerformanceLHigh,
     CaseType.PerformanceMHigh: PerformanceMHigh,
-    CaseType.PerformanceSHigh: PerformanceSHigh,
 }
