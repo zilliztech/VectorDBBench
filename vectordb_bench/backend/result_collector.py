954261b22c50f9e754389445a76e6e134db05bd9
min.tian <min.tian.cn@gmail.com>
2023-06-29 15:35:07 +0800

    add dbPrices.json to save prices data
    Signed-off-by: min.tian <min.tian.cn@gmail.com>

--- vectordb_bench/backend/result_collector.py
+++ vectordb_bench/backend/result_collector.py
@@ -6,10 +6,10 @@ class ResultCollector:
     @classmethod
     def collect(cls, result_dir: pathlib.Path) -> list[TestResult]:
         results = []
-        if not result_dir.exists() or len(list(result_dir.glob("*.json"))) == 0:
+        if not result_dir.exists() or len(list(result_dir.glob("result_*.json"))) == 0:
             return []
 
-        for json_file in result_dir.glob("*.json"):
+        for json_file in result_dir.glob("result_*.json"):
             results.append(TestResult.read_file(json_file, trans_unit=True))
 
         return results
--- vectordb_bench/frontend/const/dbPrices.py
+++ vectordb_bench/frontend/const/dbPrices.py
@@ -1,34 +1,6 @@
-from vectordb_bench.backend.clients import DB
+from vectordb_bench import config
+import ujson
+import pathlib
 
-
-DB_DBLABEL_TO_PRICE = {
-    DB.Milvus.value: {},
-    DB.ZillizCloud.value: {
-        "1cu-perf": 0.159,
-        "8cu-perf": 1.272,
-        "1cu-cap": 0.159,
-        "2cu-cap": 0.318,
-    },
-    DB.WeaviateCloud.value: {
-        # "sandox": 0,
-        "standard": 10.10,
-        "bus_crit": 32.60,
-    },
-    DB.ElasticCloud.value: {
-        "upTo2.5c8g": 0.4793,
-    },
-    DB.QdrantCloud.value: {
-        "0.5c4g-1node": 0.052,
-        "2c8g-1node": 0.166,
-        "4c16g-5node": 1.426,
-    },
-    DB.Pinecone.value: {
-        "s1.x1": 0.0973,
-        "s1.x2": 0.194,
-        "p1.x1": 0.0973,
-        "p2.x1": 0.146,
-        "p2.x1-8node": 1.168,
-        "p1.x1-8node": 0.779,
-        "s1.x1-2node": 0.195,
-    },
-}
+with open(pathlib.Path(config.RESULTS_LOCAL_DIR, "dbPrices.json")) as f:
+    DB_DBLABEL_TO_PRICE = ujson.load(f)
