fca65bd09e2c09c0cfa2a84458d9b06c4a8fd8fe
Slayden Gruneberg <slayden.gruneberg@berkeley.edu>
2023-07-13 14:27:10 -0700

    redis draft complete

--- vectordb_bench/frontend/const/styles.py
+++ vectordb_bench/frontend/const/styles.py
@@ -43,6 +43,7 @@ DB_TO_ICON = {
     DB.QdrantCloud: "https://assets.zilliz.com/qdrant_b691674fcd.png",
     DB.WeaviateCloud: "https://assets.zilliz.com/weaviate_4f6f171ebe.png",
     DB.PgVector: "https://assets.zilliz.com/PG_Vector_d464f2ef5f.png",
+    DB.Redis: "https://assets.zilliz.com/Redis_Cloud_74b8bfef39.png",
     DB.Chroma: "https://assets.zilliz.com/chroma_ceb3f06ed7.png", 
 }
 
@@ -56,4 +57,5 @@ COLOR_MAP = {
     DB.QdrantCloud.value: "#D91AD9",
     DB.WeaviateCloud.value: "#20C997",
     DB.PgVector.value: "#4C779A",
+    DB.Redis.value: "#0D6EFD",
 }
