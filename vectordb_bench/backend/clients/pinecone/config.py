6939b91e120475b9e2372851c7f8d0ca475eb7b7
min.tian <min.tian.cn@gmail.com>
2024-10-28 14:24:52 +0800

    fix pinecone client
    Signed-off-by: min.tian <min.tian.cn@gmail.com>

--- vectordb_bench/backend/clients/pinecone/config.py
+++ vectordb_bench/backend/clients/pinecone/config.py
@@ -4,12 +4,10 @@ from ..api import DBConfig
 
 class PineconeConfig(DBConfig):
     api_key: SecretStr
-    environment: SecretStr
     index_name: str
 
     def to_dict(self) -> dict:
         return {
             "api_key": self.api_key.get_secret_value(),
-            "environment": self.environment.get_secret_value(),
             "index_name": self.index_name,
         }
