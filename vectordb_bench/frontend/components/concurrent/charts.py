f0a0d1fd97f683e5f84c4b50b8c54424357e06db
min.tian <min.tian.cn@gmail.com>
2024-11-25 10:42:53 +0800

    fix conc_latency_p99 calculation; add conc_latency_avg metric; conc_test first
    Signed-off-by: min.tian <min.tian.cn@gmail.com>

--- vectordb_bench/frontend/components/concurrent/charts.py
+++ vectordb_bench/frontend/components/concurrent/charts.py
@@ -6,7 +6,7 @@ import plotly.express as px
 from vectordb_bench.frontend.config.styles import COLOR_MAP
 
 
-def drawChartsByCase(allData, showCaseNames: list[str], st):
+def drawChartsByCase(allData, showCaseNames: list[str], st, latency_type: str):
     initMainExpanderStyle(st)
     for caseName in showCaseNames:
         chartContainer = st.expander(caseName, True)
@@ -14,15 +14,24 @@ def drawChartsByCase(allData, showCaseNames: list[str], st):
         data = [
             {
                 "conc_num": caseData["conc_num_list"][i],
-                "qps": caseData["conc_qps_list"][i],
-                "latency_p99": caseData["conc_latency_p99_list"][i] * 1000,
+                "qps": caseData["conc_qps_list"][i]
+                if 0 <= i < len(caseData["conc_qps_list"])
+                else 0,
+                "latency_p99": caseData["conc_latency_p99_list"][i] * 1000
+                if 0 <= i < len(caseData["conc_latency_p99_list"])
+                else 0,
+                "latency_avg": caseData["conc_latency_avg_list"][i] * 1000
+                if 0 <= i < len(caseData["conc_latency_avg_list"])
+                else 0,
                 "db_name": caseData["db_name"],
                 "db": caseData["db"],
             }
             for caseData in caseDataList
             for i in range(len(caseData["conc_num_list"]))
         ]
-        drawChart(data, chartContainer, key=f"{caseName}-qps-p99")
+        drawChart(
+            data, chartContainer, key=f"{caseName}-qps-p99", x_metric=latency_type
+        )
 
 
 def getRange(metric, data, padding_multipliers):
@@ -36,14 +45,21 @@ def getRange(metric, data, padding_multipliers):
     return rangeV
 
 
-def drawChart(data, st, key: str):
+def gen_title(s: str) -> str:
+    if "latency" in s:
+        return f'{s.replace("_", " ").title()} (ms)'
+    else:
+        return s.upper()
+
+
+def drawChart(data, st, key: str, x_metric: str = "latency_p99", y_metric: str = "qps"):
     if len(data) == 0:
         return
 
-    x = "latency_p99"
+    x = x_metric
     xrange = getRange(x, data, [0.05, 0.1])
 
-    y = "qps"
+    y = y_metric
     yrange = getRange(y, data, [0.2, 0.1])
 
     color = "db"
@@ -69,8 +85,8 @@ def drawChart(data, st, key: str):
         },
         height=720,
     )
-    fig.update_xaxes(range=xrange, title_text="Latency P99 (ms)")
-    fig.update_yaxes(range=yrange, title_text="QPS")
+    fig.update_xaxes(range=xrange, title_text=gen_title(x_metric))
+    fig.update_yaxes(range=yrange, title_text=gen_title(y_metric))
     fig.update_traces(textposition="bottom right", texttemplate="conc-%{text:,.4~r}")
 
     st.plotly_chart(fig, use_container_width=True, key=key)
