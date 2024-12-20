40a98ea59ef4689abd961053df68e3b40d696585
wxywb <xy.wang@zilliz.com>
2024-12-20 07:35:29 +0000

    feat: add label filtering to pinecone client.
    Signed-off-by: wxywb <xy.wang@zilliz.com>

--- vectordb_bench/backend/clients/pinecone/pinecone.py
+++ vectordb_bench/backend/clients/pinecone/pinecone.py
@@ -4,6 +4,7 @@ import logging
 from contextlib import contextmanager
 from typing import Type
 import pinecone
+from vectordb_bench.backend.filter import Filter, FilterType
 from ..api import VectorDB, DBConfig, DBCaseConfig, EmptyDBCaseConfig, IndexType
 from .config import PineconeConfig
 
@@ -15,12 +16,18 @@ PINECONE_MAX_SIZE_PER_BATCH = 2 * 1024 * 1024  # 2MB
 
 
 class Pinecone(VectorDB):
+    supported_filter_types: list[FilterType] = [
+        FilterType.NonFilter,
+        FilterType.Int,
+        FilterType.Label,
+    ]
     def __init__(
         self,
         dim,
         db_config: dict,
         db_case_config: DBCaseConfig,
         drop_old: bool = False,
+        with_scalar_labels: bool = False,
         **kwargs,
     ):
         """Initialize wrapper around the milvus vector database."""
@@ -33,6 +40,7 @@ class Pinecone(VectorDB):
         pc = pinecone.Pinecone(api_key=self.api_key)
         index = pc.Index(self.index_name)
 
+        self.with_scalar_labels = with_scalar_labels
         if drop_old:
             index_stats = index.describe_index_stats()
             index_dim = index_stats["dimension"]
@@ -45,6 +53,7 @@ class Pinecone(VectorDB):
                 index.delete(delete_all=True, namespace=namespace)
 
         self._metadata_key = "meta"
+        self._scalar_label_field = "label"
 
     @classmethod
     def config_cls(cls) -> Type[DBConfig]:
@@ -70,6 +79,7 @@ class Pinecone(VectorDB):
         self,
         embeddings: list[list[float]],
         metadata: list[int],
+        labels_data: list[str] = None,
         **kwargs,
     ) -> (int, Exception):
         assert len(embeddings) == len(metadata)
@@ -81,10 +91,13 @@ class Pinecone(VectorDB):
                 )
                 insert_datas = []
                 for i in range(batch_start_offset, batch_end_offset):
+                    metadata_dict = {self._metadata_key: metadata[i]}
+                    if self.with_scalar_labels:
+                        metadata_dict[self._scalar_label_field] = labels_data[i]
                     insert_data = (
                         str(metadata[i]),
                         embeddings[i],
-                        {self._metadata_key: metadata[i]},
+                        metadata_dict,
                     )
                     insert_datas.append(insert_data)
                 self.index.upsert(insert_datas)
@@ -97,13 +110,9 @@ class Pinecone(VectorDB):
         self,
         query: list[float],
         k: int = 100,
-        filters: dict | None = None,
         timeout: int | None = None,
     ) -> list[int]:
-        if filters is None:
-            pinecone_filters = {}
-        else:
-            pinecone_filters = {self._metadata_key: {"$gte": filters["id"]}}
+        pinecone_filters = self.expr
         try:
             res = self.index.query(
                 top_k=k,
@@ -115,3 +124,13 @@ class Pinecone(VectorDB):
             raise e
         id_res = [int(one_res["id"]) for one_res in res]
         return id_res
+
+    def prepare_filter(self, filter: Filter):
+        if filter.type == FilterType.NonFilter:
+            self.expr = None
+        elif filter.type == FilterType.Int:
+            self.expr = {self._scalar_id_field: {"$gte": filter.int_value}}
+        elif filter.type == FilterType.Label:
+            self.expr = {self._scalar_label_field : {"$eq": filter.label_value}}
+        else:
+            raise ValueError(f"Not support Filter for Pinecone - {filter}")
