b46ac1f6121b50abf8b58937e8acc2464a2f4808
min.tian <min.tian.cn@gmail.com>
2025-02-16 23:16:41 +0800

    label fitler for Qdrant
    Signed-off-by: min.tian <min.tian.cn@gmail.com>

--- vectordb_bench/backend/clients/qdrant_cloud/config.py
+++ vectordb_bench/backend/clients/qdrant_cloud/config.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, SecretStr, validator
+from qdrant_client.http.models import QuantizationSearchParams, SearchParams
 
 from ..api import DBCaseConfig, DBConfig, MetricType
 
@@ -22,15 +23,41 @@ class QdrantConfig(DBConfig):
 
     @validator("*")
     def not_empty_field(cls, v: any, field: any):
-        if field.name in ["api_key", "db_label"]:
+        if field.name in ["api_key"]:
             return v
-        if isinstance(v, str | SecretStr) and len(v) == 0:
-            raise ValueError("Empty string!")
-        return v
+        return super().not_empty_field(v, field)
 
 
 class QdrantIndexConfig(BaseModel, DBCaseConfig):
     metric_type: MetricType | None = None
+    m: int = 16
+    payload_m: int = 16  # only for label_filter cases
+    create_payload_int_index: bool = False
+    create_payload_keyword_index: bool = False
+    is_tenant: bool = False
+    use_scalar_quant: bool = False
+    sq_quantile: float = 0.99
+    default_segment_number: int = 0
+
+    use_rescore: bool = False
+    oversampling: float = 1.0
+    indexed_only: bool = False
+    hnsw_ef: int | None = 100
+    exact: bool = False
+
+    with_payload: bool = False
+
+    def __eq__(self, obj: any):
+        return (
+            self.m == obj.m
+            and self.payload_m == obj.payload_m
+            and self.create_payload_int_index == obj.create_payload_int_index
+            and self.create_payload_keyword_index == obj.create_payload_keyword_index
+            and self.is_tenant == obj.is_tenant
+            and self.use_scalar_quant == obj.use_scalar_quant
+            and self.sq_quantile == obj.sq_quantile
+            and self.default_segment_number == obj.default_segment_number
+        )
 
     def parse_metric(self) -> str:
         if self.metric_type == MetricType.L2:
@@ -44,5 +71,19 @@ class QdrantIndexConfig(BaseModel, DBCaseConfig):
     def index_param(self) -> dict:
         return {"distance": self.parse_metric()}
 
-    def search_param(self) -> dict:
-        return {}
+    def search_param(self) -> SearchParams:
+        quantization = (
+            QuantizationSearchParams(
+                ignore=False,
+                rescore=True,
+                oversampling=self.oversampling,
+            )
+            if self.use_rescore
+            else None
+        )
+        return SearchParams(
+            hnsw_ef=self.hnsw_ef,
+            exact=self.exact,
+            indexed_only=self.indexed_only,
+            quantization=quantization,
+        )
