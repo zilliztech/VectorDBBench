e50898f451cfe961ad484abeb692761ae66e97dd
min.tian <min.tian.cn@gmail.com>
2025-02-25 10:14:57 +0800

    label filter for AWS OpenSearch
    Signed-off-by: min.tian <min.tian.cn@gmail.com>

--- vectordb_bench/backend/clients/aws_opensearch/aws_opensearch.py
+++ vectordb_bench/backend/clients/aws_opensearch/aws_opensearch.py
@@ -5,8 +5,10 @@ from contextlib import contextmanager
 
 from opensearchpy import OpenSearch
 
-from ..api import IndexType, VectorDB
-from .config import AWSOpenSearchConfig, AWSOpenSearchIndexConfig, AWSOS_Engine
+from vectordb_bench.backend.filter import Filter, FilterOp
+
+from ..api import VectorDB
+from .config import AWSOpenSearchIndexConfig, AWSOS_Engine
 
 log = logging.getLogger(__name__)
 
@@ -16,15 +18,23 @@ SECONDS_WAITING_FOR_REPLICAS_TO_BE_ENABLED_SEC = 30
 
 
 class AWSOpenSearch(VectorDB):
+    supported_filter_types: list[FilterOp] = [
+        FilterOp.NonFilter,
+        FilterOp.NumGE,
+        FilterOp.StrEqual,
+    ]
+
     def __init__(
         self,
         dim: int,
         db_config: dict,
         db_case_config: AWSOpenSearchIndexConfig,
         index_name: str = "vdb_bench_index",  # must be lowercase
-        id_col_name: str = "_id",
+        id_col_name: str = "id",
+        label_col_name: str = "label",
         vector_col_name: str = "embedding",
         drop_old: bool = False,
+        with_scalar_labels: bool = False,
         **kwargs,
     ):
         self.dim = dim
@@ -32,8 +42,9 @@ class AWSOpenSearch(VectorDB):
         self.case_config = db_case_config
         self.index_name = index_name
         self.id_col_name = id_col_name
-        self.category_col_names = [f"scalar-{categoryCount}" for categoryCount in [2, 5, 10, 100, 1000]]
+        self.label_col_name = label_col_name
         self.vector_col_name = vector_col_name
+        self.with_scalar_labels = with_scalar_labels
 
         log.info(f"AWS_OpenSearch client config: {self.db_config}")
         client = OpenSearch(**self.db_config)
@@ -44,13 +55,9 @@ class AWSOpenSearch(VectorDB):
                 client.indices.delete(index=self.index_name)
             self._create_index(client)
 
-    @classmethod
-    def config_cls(cls) -> AWSOpenSearchConfig:
-        return AWSOpenSearchConfig
-
-    @classmethod
-    def case_config_cls(cls, index_type: IndexType | None = None) -> AWSOpenSearchIndexConfig:
-        return AWSOpenSearchIndexConfig
+    def need_normalize_cosine(self):
+        # currently faiss doesn't support cosine
+        return self.case_config.engine == AWSOS_Engine.faiss
 
     def _create_index(self, client: OpenSearch):
         cluster_settings_body = {
@@ -65,19 +72,15 @@ class AWSOpenSearch(VectorDB):
                 "knn": True,
                 "number_of_shards": self.case_config.number_of_shards,
                 "number_of_replicas": 0,
-                "translog.flush_threshold_size": self.case_config.flush_threshold_size,
                 # Setting trans log threshold to 5GB
-                **(
-                    {"knn.algo_param.ef_search": self.case_config.ef_search}
-                    if self.case_config.engine == AWSOS_Engine.nmslib
-                    else {}
-                ),
+                "translog.flush_threshold_size": self.case_config.flush_threshold_size,
             },
             "refresh_interval": self.case_config.refresh_interval,
         }
         mappings = {
             "properties": {
-                **{categoryCol: {"type": "keyword"} for categoryCol in self.category_col_names},
+                self.id_col_name: {"type": "integer", "store": True},
+                self.label_col_name: {"type": "keyword"},
                 self.vector_col_name: {
                     "type": "knn_vector",
                     "dimension": self.dim,
@@ -107,6 +110,7 @@ class AWSOpenSearch(VectorDB):
         self,
         embeddings: Iterable[list[float]],
         metadata: list[int],
+        labels_data: list[str] | None = None,
         **kwargs,
     ) -> tuple[int, Exception]:
         """Insert the embeddings to the opensearch."""
@@ -115,61 +119,91 @@ class AWSOpenSearch(VectorDB):
         insert_data = []
         for i in range(len(embeddings)):
             insert_data.append(
-                {"index": {"_index": self.index_name, self.id_col_name: metadata[i]}},
+                {"index": {"_index": self.index_name, "routing": labels_data[i]}}
+                if self.with_scalar_labels and self.case_config.use_routing
+                else {"index": {"_index": self.index_name}}
             )
-            insert_data.append({self.vector_col_name: embeddings[i]})
+
+            data = {
+                self.id_col_name: metadata[i],
+                self.vector_col_name: embeddings[i],
+            }
+            if self.with_scalar_labels:
+                data[self.label_col_name] = labels_data[i]
+
+            insert_data.append(data)
+
         try:
-            resp = self.client.bulk(insert_data)
-            log.info(f"AWS_OpenSearch adding documents: {len(resp['items'])}")
-            resp = self.client.indices.stats(self.index_name)
-            log.info(
-                f"Total document count in index: {resp['_all']['primaries']['indexing']['index_total']}",
-            )
-            return (len(embeddings), None)
+            self.client.bulk(insert_data)
+            return len(embeddings), None
         except Exception as e:
             log.warning(f"Failed to insert data: {self.index_name} error: {e!s}")
-            time.sleep(10)
-            return self.insert_embeddings(embeddings, metadata)
+            return 0, e
 
     def search_embedding(
         self,
         query: list[float],
         k: int = 100,
-        filters: dict | None = None,
+        **kwargs,
     ) -> list[int]:
         """Get k most similar embeddings to query vector.
 
         Args:
             query(list[float]): query embedding to look up documents similar to.
             k(int): Number of most similar embeddings to return. Defaults to 100.
-            filters(dict, optional): filtering expression to filter the data while searching.
 
         Returns:
-            list[tuple[int, float]]: list of k most similar embeddings in (id, score) tuple to the query embedding.
+            list[int]: list of k most similar ids to the query embedding.
         """
         assert self.client is not None, "should self.init() first"
 
         body = {
             "size": k,
-            "query": {"knn": {self.vector_col_name: {"vector": query, "k": k}}},
-            **({"filter": {"range": {self.id_col_name: {"gt": filters["id"]}}}} if filters else {}),
+            "query": {
+                "knn": {
+                    self.vector_col_name: {
+                        "vector": query,
+                        "k": k,
+                        "method_parameters": self.case_config.search_param(),
+                        **({"filter": self.filter} if self.filter else {}),
+                        **(
+                            {"rescore": {"oversample_factor": self.case_config.oversample_factor}}
+                            if self.case_config.use_quant
+                            else {}
+                        ),
+                    }
+                }
+            },
         }
+        resp = self.client.search(
+            index=self.index_name,
+            body=body,
+            size=k,
+            _source=False,
+            docvalue_fields=[self.id_col_name],
+            stored_fields="_none_",
+            filter_path=[f"hits.hits.fields.{self.id_col_name}"],
+            routing=self.routing_key,
+        )
         try:
-            resp = self.client.search(
-                index=self.index_name,
-                body=body,
-                size=k,
-                _source=False,
-                docvalue_fields=[self.id_col_name],
-                stored_fields="_none_",
-            )
-            log.debug(f"Search took: {resp['took']}")
-            log.debug(f"Search shards: {resp['_shards']}")
-            log.debug(f"Search hits total: {resp['hits']['total']}")
             return [int(h["fields"][self.id_col_name][0]) for h in resp["hits"]["hits"]]
-        except Exception as e:
-            log.warning(f"Failed to search: {self.index_name} error: {e!s}")
-            raise e from None
+        except Exception:
+            # empty resutls
+            return []
+
+    def prepare_filter(self, filters: Filter):
+        self.routing_key = None
+        if filters.type == FilterOp.NonFilter:
+            self.filter = None
+        elif filters.type == FilterOp.NumGE:
+            self.filter = {"range": {self.id_col_name: {"gt": filters.int_value}}}
+        elif filters.type == FilterOp.StrEqual:
+            self.filter = {"term": {self.label_col_name: filters.label_value}}
+            if self.case_config.use_routing:
+                self.routing_key = filters.label_value
+        else:
+            msg = f"Not support Filter for OpenSearch - {filters}"
+            raise ValueError(msg)
 
     def optimize(self, data_size: int | None = None):
         """optimize will be called between insertion and search in performance cases."""
@@ -200,7 +234,7 @@ class AWSOpenSearch(VectorDB):
         while True:
             res = self.client.cat.indices(index=self.index_name, h="health", format="json")
             health = res[0]["health"]
-            if health != "green":
+            if health == "green":
                 break
             log.info(f"The index {self.index_name} has health : {health} and is not green. Retrying")
             time.sleep(SECONDS_WAITING_FOR_REPLICAS_TO_BE_ENABLED_SEC)
@@ -228,15 +262,19 @@ class AWSOpenSearch(VectorDB):
             "persistent": {"knn.algo_param.index_thread_qty": self.case_config.index_thread_qty_during_force_merge}
         }
         self.client.cluster.put_settings(cluster_settings_body)
-        log.debug(f"Starting force merge for index {self.index_name}")
-        force_merge_endpoint = f"/{self.index_name}/_forcemerge?max_num_segments=1&wait_for_completion=false"
+        log.info(f"Starting force merge for index {self.index_name}")
+        force_merge_endpoint = (
+            f"/{self.index_name}/_forcemerge?"
+            f"max_num_segments={self.case_config.number_of_segments}"
+            f"&wait_for_completion=false"
+        )
         force_merge_task_id = self.client.transport.perform_request("POST", force_merge_endpoint)["task"]
         while True:
             time.sleep(WAITING_FOR_FORCE_MERGE_SEC)
             task_status = self.client.tasks.get(task_id=force_merge_task_id)
             if task_status["completed"]:
                 break
-        log.debug(f"Completed force merge for index {self.index_name}")
+        log.info(f"Completed force merge for index {self.index_name}")
 
     def _load_graphs_to_memory(self):
         if self.case_config.engine != AWSOS_Engine.lucene:
