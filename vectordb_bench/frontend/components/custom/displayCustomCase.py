cb4712c939a7205679843d48fdf1bde80844733d
siqi.an <ansiqi_7777@163.com>
2025-02-27 16:30:37 +0800

    custom control file and column name
    custom control file and column name
    
    custom control file and column name
    
    custom control file and column name
    
    custom control name of file and column

--- vectordb_bench/frontend/components/custom/displayCustomCase.py
+++ vectordb_bench/frontend/components/custom/displayCustomCase.py
@@ -23,7 +23,38 @@ def displayCustomCase(customCase: CustomCaseConfig, st, key):
         "metric type", key=f"{key}_metric_type", options=["L2", "Cosine", "IP"]
     )
     customCase.dataset_config.file_count = columns[3].number_input(
-        "train file count", key=f"{key}_file_count", value=customCase.dataset_config.file_count
+        "train file count",
+        key=f"{key}_file_count",
+        value=customCase.dataset_config.file_count,
+        help="if train file count is more than one, please input all your train file name and split with ','",
+    )
+
+    columns = st.columns(3)
+    customCase.dataset_config.train_name = columns[0].text_input(
+        "train file name",
+        key=f"{key}_train_name",
+        value=customCase.dataset_config.train_name,
+        help="if your file and column in the file is not named as previous explanation, please input the real name (for example: if the file name is `tr.parquet` and column name is `embbb`, then input tr and embbb)",
+    )
+    customCase.dataset_config.test_name = columns[1].text_input(
+        "test file name", key=f"{key}_test_name", value=customCase.dataset_config.test_name
+    )
+    customCase.dataset_config.gt_name = columns[2].text_input(
+        "ground truth file name", key=f"{key}_gt_name", value=customCase.dataset_config.gt_name
+    )
+
+    columns = st.columns([1, 1, 2, 2])
+    customCase.dataset_config.train_id_name = columns[0].text_input(
+        "train id name", key=f"{key}_train_id_name", value=customCase.dataset_config.train_id_name
+    )
+    customCase.dataset_config.train_col_name = columns[1].text_input(
+        "train emb name", key=f"{key}_train_col_name", value=customCase.dataset_config.train_col_name
+    )
+    customCase.dataset_config.test_col_name = columns[2].text_input(
+        "test emb name", key=f"{key}_test_col_name", value=customCase.dataset_config.test_col_name
+    )
+    customCase.dataset_config.gt_col_name = columns[3].text_input(
+        "ground truth emb name", key=f"{key}_gt_col_name", value=customCase.dataset_config.gt_col_name
     )
 
     columns = st.columns(4)
