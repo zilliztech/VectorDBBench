2b709519b12d6c760ad14e22c92137376688e1f0
ZebinRen <80635329+ZebinRen@users.noreply.github.com>
2025-06-11 08:38:40 +0200

    expose lancedb index parameters to the cli interface (#537)
    * expose lancedb index parameters to the cli interface
    
    * fix formatting issues

--- vectordb_bench/backend/clients/lancedb/cli.py
+++ vectordb_bench/backend/clients/lancedb/cli.py
@@ -58,10 +58,46 @@ def LanceDBAutoIndex(**parameters: Unpack[LanceDBTypedDict]):
     )
 
 
+class LanceDBIVFPQTypedDict(CommonTypedDict, LanceDBTypedDict):
+    num_partitions: Annotated[
+        int,
+        click.option(
+            "--num-partitions",
+            type=int,
+            default=0,
+            help="Number of partitions for IVFPQ index, unset = use LanceDB default",
+        ),
+    ]
+    num_sub_vectors: Annotated[
+        int,
+        click.option(
+            "--num-sub-vectors",
+            type=int,
+            default=0,
+            help="Number of sub-vectors for IVFPQ index, unset = use LanceDB default",
+        ),
+    ]
+    nbits: Annotated[
+        int,
+        click.option(
+            "--nbits",
+            type=int,
+            default=8,
+            help="Number of bits for IVFPQ index (must be 4 or 8), unset = use LanceDB default",
+        ),
+    ]
+    nprobes: Annotated[
+        int,
+        click.option(
+            "--nprobes", type=int, default=0, help="Number of probes for IVFPQ search, unset = use LanceDB default"
+        ),
+    ]
+
+
 @cli.command()
-@click_parameter_decorators_from_typed_dict(LanceDBTypedDict)
-def LanceDBIVFPQ(**parameters: Unpack[LanceDBTypedDict]):
-    from .config import LanceDBConfig, _lancedb_case_config
+@click_parameter_decorators_from_typed_dict(LanceDBIVFPQTypedDict)
+def LanceDBIVFPQ(**parameters: Unpack[LanceDBIVFPQTypedDict]):
+    from .config import LanceDBConfig, LanceDBIndexConfig
 
     run(
         db=DB.LanceDB,
@@ -70,15 +106,29 @@ def LanceDBIVFPQ(**parameters: Unpack[LanceDBTypedDict]):
             uri=parameters["uri"],
             token=SecretStr(parameters["token"]) if parameters.get("token") else None,
         ),
-        db_case_config=_lancedb_case_config.get(IndexType.IVFPQ)(),
+        db_case_config=LanceDBIndexConfig(
+            index=IndexType.IVFPQ,
+            num_partitions=parameters["num_partitions"],
+            num_sub_vectors=parameters["num_sub_vectors"],
+            nbits=parameters["nbits"],
+            nprobes=parameters["nprobes"],
+        ),
         **parameters,
     )
 
 
+class LanceDBHNSWTypedDict(CommonTypedDict, LanceDBTypedDict):
+    m: Annotated[int, click.option("--m", type=int, default=0, help="HNSW parameter m")]
+    ef_construction: Annotated[
+        int, click.option("--ef-construction", type=int, default=0, help="HNSW parameter ef_construction")
+    ]
+    ef: Annotated[int, click.option("--ef", type=int, default=0, help="HNSW search parameter ef")]
+
+
 @cli.command()
-@click_parameter_decorators_from_typed_dict(LanceDBTypedDict)
-def LanceDBHNSW(**parameters: Unpack[LanceDBTypedDict]):
-    from .config import LanceDBConfig, _lancedb_case_config
+@click_parameter_decorators_from_typed_dict(LanceDBHNSWTypedDict)
+def LanceDBHNSW(**parameters: Unpack[LanceDBHNSWTypedDict]):
+    from .config import LanceDBConfig, LanceDBHNSWIndexConfig
 
     run(
         db=DB.LanceDB,
@@ -87,6 +137,10 @@ def LanceDBHNSW(**parameters: Unpack[LanceDBTypedDict]):
             uri=parameters["uri"],
             token=SecretStr(parameters["token"]) if parameters.get("token") else None,
         ),
-        db_case_config=_lancedb_case_config.get(IndexType.HNSW)(),
+        db_case_config=LanceDBHNSWIndexConfig(
+            m=parameters["m"],
+            ef_construction=parameters["ef_construction"],
+            ef=parameters["ef"],
+        ),
         **parameters,
     )
