62da21e4b17ad03cc14b056433df14f47a501238
acanadil <albertcanadilla@gmail.com>
2025-09-12 12:41:55 +0200

    Fix linting with ruff --fix and black.
    Co-authored-by: Enrique enrique.molina@urv.cat
    Co-authored-by: Octavio octaviohoracio.iacoponelli@urv.cat

--- vectordb_bench/backend/clients/s3_vectors/cli.py
+++ vectordb_bench/backend/clients/s3_vectors/cli.py
@@ -11,19 +11,31 @@ from ....cli.cli import (
     run,
 )
 from .. import DB
-from .config import S3VectorsIndexConfig
 from ..api import MetricType
+from .config import S3VectorsIndexConfig
 
 
 class S3VectorsTypedDict(TypedDict):
-    region_name: Annotated[str, click.option("--region", type=str, help="AWS region for S3 bucket (eg. us-east-1)", default="us-east-1")]
+    region_name: Annotated[
+        str, click.option("--region", type=str, help="AWS region for S3 bucket (eg. us-east-1)", default="us-east-1")
+    ]
     access_key_id: Annotated[str, click.option("--access_key_id", type=str, help="AWS access key ID", required=True)]
-    secret_access_key: Annotated[str, click.option("--secret_access_key", type=str, help="AWS secret access key", required=True)]
+    secret_access_key: Annotated[
+        str, click.option("--secret_access_key", type=str, help="AWS secret access key", required=True)
+    ]
 
     bucket: Annotated[str, click.option("--bucket", type=str, help="S3 bucket name", required=True)]
     index: Annotated[str, click.option("--index", type=str, help="Unique vector index name", default="vdbbench-index")]
 
-    metric: Annotated[str, click.option("--metric", type=str, help="Distance metric for vector similarity (e.g., 'cosine', 'euclidean').", default=None)]
+    metric: Annotated[
+        str,
+        click.option(
+            "--metric",
+            type=str,
+            help="Distance metric for vector similarity (e.g., 'cosine', 'euclidean').",
+            default=None,
+        ),
+    ]
 
 
 class S3VectorsIndexTypedDict(CommonTypedDict, S3VectorsTypedDict): ...
@@ -45,8 +57,11 @@ def S3Vectors(**parameters: Unpack[S3VectorsIndexTypedDict]):
             index_name=parameters["index"] if parameters["index"] else "vdbbench-index",
         ),
         db_case_config=S3VectorsIndexConfig(
-            metric_type= MetricType.COSINE if parameters["metric"] == "cosine" else MetricType.L2 if parameters["metric"] == "l2" else None
+            metric_type=(
+                MetricType.COSINE
+                if parameters["metric"] == "cosine"
+                else MetricType.L2 if parameters["metric"] == "l2" else None
+            )
         ),
         **parameters,
     )
-
