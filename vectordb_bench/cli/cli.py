3ba2622a10c2c0ccbdb7deb31599dddb9a65144c
egolearner <egolearner@outlook.com>
2025-11-07 11:11:45 +0800

    fix format

--- vectordb_bench/cli/cli.py
+++ vectordb_bench/cli/cli.py
@@ -430,9 +430,9 @@ class CommonTypedDict(TypedDict):
         str,
         click.option(
             "--dataset-with-size-type",
-            help="Dataset with size type for NewIntFilterPerformanceCase/LabelFilterPerformanceCase, you can use Medium Cohere (768dim, 1M)|"
-            "Large Cohere (768dim, 10M)|Medium Bioasq (1024dim, 1M)|Large Bioasq (1024dim, 10M)|"
-            "Large OpenAI (1536dim, 5M)|Medium OpenAI (1536dim, 500K)",
+            help="Dataset with size type for NewIntFilterPerformanceCase/LabelFilterPerformanceCase, you can use "
+            "Medium Cohere (768dim, 1M)|Large Cohere (768dim, 10M)|Medium Bioasq (1024dim, 1M)|"
+            "Large Bioasq (1024dim, 10M)|Large OpenAI (1536dim, 5M)|Medium OpenAI (1536dim, 500K)",
             default="Medium Cohere (768dim, 1M)",
             show_default=True,
         ),
