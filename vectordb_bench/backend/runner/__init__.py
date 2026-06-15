bf6ab9373a93b246053afee1530b5c779fa4c646
jamesgao-jpg <james.gao@zilliz.com>
2026-06-15 20:41:40 +0800

    Update runner package exports after FTS refactor
    Signed-off-by: jamesgao-jpg <james.gao@zilliz.com>

--- vectordb_bench/backend/runner/__init__.py
+++ vectordb_bench/backend/runner/__init__.py
@@ -2,14 +2,13 @@ from .cold_warm_runner import ColdWarmSearchRunner
 from .concurrent_runner import ConcurrentInsertRunner
 from .mp_runner import MultiProcessingSearchRunner
 from .read_write_runner import ReadWriteRunner
-from .serial_runner import SerialFtsInsertRunner, SerialInsertRunner, SerialSearchRunner
+from .serial_runner import SerialInsertRunner, SerialSearchRunner
 
 __all__ = [
     "ColdWarmSearchRunner",
     "ConcurrentInsertRunner",
     "MultiProcessingSearchRunner",
     "ReadWriteRunner",
-    "SerialFtsInsertRunner",
     "SerialInsertRunner",
     "SerialSearchRunner",
 ]
